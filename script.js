1514c5c5e095acc988827a02fbeb956b99d31b69
iamviveksingh00 <142625911+iamviveksingh00@users.noreply.github.com>
2025-02-28 21:08:30 +0530

    Updated portfolio website

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.querySelector(".theme-toggle");
-    themeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-    });
-});
