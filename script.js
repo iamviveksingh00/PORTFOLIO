804a0c38d8c2811323f6750c39a78e53b824afbc
iamviveksingh00 <142625911+iamviveksingh00@users.noreply.github.com>
2025-02-28 20:33:23 +0530

    Added multiple pages and navbar

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// Typing Effect
-var typed = new Typed(".typing", {
-    strings: ["John Doe", "a Web Developer", "an AI Engineer"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    loop: true
-});
-
-// Dark Mode Toggle
-const toggleButton = document.getElementById("theme-toggle");
-toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.querySelector(".theme-toggle");
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
 });
