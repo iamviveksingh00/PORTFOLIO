a4a11968cff9387ca4124c08dfdbeebb604711e7
iamviveksingh00 <142625911+iamviveksingh00@users.noreply.github.com>
2025-03-02 00:23:50 +0530

    Updated portfolio website

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Website Loaded Successfully!");
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-    let currentLocation = window.location.pathname.split("/").pop();
-    let navLinks = document.querySelectorAll("nav ul li a");
-
-    navLinks.forEach(link => {
-        if (link.getAttribute("href") === currentLocation) {
-            link.classList.add("active");
-        } else {
-            link.classList.remove("active");
-        }
-    });
-});
