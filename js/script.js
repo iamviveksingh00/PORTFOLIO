960f74e353d250f50cd67082a888d5c16bdfdbb7
iamviveksingh00 <142625911+iamviveksingh00@users.noreply.github.com>
2025-03-14 22:32:13 +0530

    Updated portfolio website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Website Loaded Successfully!");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = "1"; // Ensure full opacity after loading
+});
